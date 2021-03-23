8d4e18ea75ef7a8429f4e76303cfadd99ad0556b
Cian O'Sullivan <cjp1805@outlook.com>
2021-03-23 20:30:11 +0000

    Added docker microservice functionality

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ from random import randint
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = data_pb2_grpc.GetDataServiceStub(channel)
-        response = stub.GetData(data_pb2.Posts(posts=message))
+        response = stub.GetData(data_pb2.Posts(posts="Passed in String"))
     print("Client.py: ", response.received)
 
 if __name__ == "__main__":
